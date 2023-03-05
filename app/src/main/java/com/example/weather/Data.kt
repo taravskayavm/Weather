2fbf5faf976e3e286abb390e5b843ccd897a783a
taravskayavm <taravskayavm@gmail.com>
2023-03-05 21:48:36 +0300

    Data  class, Object

---
+++ app/src/main/java/com/example/weather/Data.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin_test.lesson1
+
+
+data class Data(var cityName:String, var temperature:Int)
+
+
+object Database {
+    fun getTest():String {
+        if (0==0) {
+            return "test"
+        } else {
+            return "Nope"
+        }
+
+    }
+}
