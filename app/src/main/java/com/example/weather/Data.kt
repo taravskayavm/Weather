736f186f5125fe31a8cccd634a4a9bda4d851c3e
taravskayavm <taravskayavm@gmail.com>
2023-03-08 22:29:13 +0300

    if/try/when

--- app/src/main/java/com/example/weather/Data.kt
+++ app/src/main/java/com/example/weather/Data.kt
@@ -3,14 +3,46 @@ package com.example.kotlin_test.lesson1
 
 data class Data(var cityName:String, var temperature:Int)
 
+enum class TestEnum {
+    test1,
+    test2,
+    test3,
+    test4
+}
 
 object Database {
-    fun getTest():String {
-        if (0==0) {
-            return "test"
+    fun getTestIf():String {
+
+        val result = if (0==0) {
+            val f1 = 1+23256
+            val f2 = 1+23276
+            val f3 = 1+2345
+            "test"
         } else {
-            return "Nope"
+            "Nope"
         }
 
+        try {
+
+        } catch (e:Throwable) {
+
+        }finally {
+            //закрыть соединение с сервером
+        }
+
+
+        return  result
+    }
+
+    fun getTestWhen(input:TestEnum):String {
+
+        val result = when (input) {    //switch
+            TestEnum.test1 -> "1"
+            TestEnum.test2 -> "2"
+            TestEnum.test3 -> "3"
+            TestEnum.test4 -> "4"
+        }
+
+        return  result
     }
 }
